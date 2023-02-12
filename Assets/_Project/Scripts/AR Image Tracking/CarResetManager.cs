ed4c89b76922d96a7d9756afb0cb511d0de4b871
Pappim Pipatkasrira <pappim.p@ku.th>
2023-02-12 20:43:23 +0700

    Update AR part

---
+++ Assets/_Project/Scripts/AR Image Tracking/CarResetManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using VehicleBehaviour;
+
+public class CarResetManager : MonoBehaviour
+{
+
+    public void ResetCar()
+    {
+        WheelVehicle[] vehicles = FindObjectsOfType<WheelVehicle>();
+        foreach(WheelVehicle vehicle in vehicles)
+        {
+            vehicle.transform.localPosition = Vector3.zero;
+        }
+    }
+
+}
